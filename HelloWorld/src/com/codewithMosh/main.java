c7b16c34e0b78e5ffe9da6374c8b07209c0fc7d9
Jackie <JGtheophila19@gmail.com>
2020-05-28 18:44:12 -0400

    Add files via upload

---
+++ HelloWorld/src/com/codewithMosh/main.java
@@ -0,0 +1,12 @@
+package com.codewithMosh;
+
+public class main {
+	public static void main (String[] args){
+		int x = 1;
+		int y = 1;
+		System.out.println(x == y);
+		System.out.println(x != y);
+		System.out.println(x >= y);
+		System.out.println(x <= y);
+	}
+}
